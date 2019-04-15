62047fdab91e57e44c0ebd4d9f585667042d789b
moskowitza <moskowitzaaron@mac.com>
2019-04-15 12:17:46 -0400

    signout alert

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,7 +4,20 @@ const ui = new firebaseui.auth.AuthUI(firebase.auth());
 console.log(`ui: ${ui}`);
 function googleSignin() {
         ui.start('#firebaseui-auth-container', {
-                signInSuccessUrl: '/account.html',
+                callbacks: {
+                        signInSuccessWithAuthResult(authResult, redirectUrl) {
+                                // User successfully signed in.
+                                // Return type determines whether we continue the redirect automatically
+                                // or whether we leave that to developer to handle.
+                                return true;
+                        },
+                        uiShown() {
+                                // The widget is rendered.
+                                // Hide the loader.
+                                document.getElementById('loader').style.display = 'none';
+                        },
+                },
+                signInSuccessUrl: 'https://moskowitza.github.io/firebaseLogin/account.html',
                 signInOptions: [
                         // List of OAuth providers supported.
                         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -15,7 +28,7 @@ function googleSignout() {
         firebase.auth()
                 .signOut()
                 .then(function() {
-                        // Sign-out successful.
+                        alert('Signed Out');
                 })
                 .catch(function(error) {
                         // An error happened.

acd4bd5b9685fc57bfb17b5770b8210690d41df7
moskowitza <moskowitzaaron@mac.com>
2019-04-15 12:47:57 -0400

    cleanup

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,28 +4,11 @@ const ui = new firebaseui.auth.AuthUI(firebase.auth());
 console.log(`ui: ${ui}`);
 function googleSignin() {
         ui.start('#firebaseui-auth-container', {
-                callbacks: {
-                        signInSuccessWithAuthResult(authResult, redirectUrl) {
-                                console.log(`auth result: ${authResult}`);
-                                console.log(redirectUrl);
-                                // User successfully signed in.
-                                // Return type determines whether we continue the redirect automatically
-                                // or whether we leave that to developer to handle.
-                                return true;
-                        },
-                        uiShown() {
-                                // The widget is rendered.
-                                // Hide the loader.
-                                document.getElementById('loader').style.display = 'none';
-                        },
-                },
-                signInSuccessUrl: 'https://moskowitza.github.io/firebaseLogin/account.html',
                 signInOptions: [
                         // List of OAuth providers supported.
                         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
                 ],
         });
-        checkUser(user);
 }
 function googleSignout() {
         firebase.auth()
@@ -35,6 +18,7 @@ function googleSignout() {
                 })
                 .catch(function(error) {
                         // An error happened.
+                        console.log(error);
                 });
 }
 

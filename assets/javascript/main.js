d25b3700ae2200e9f5ff1524b43facecf9ab0c83
moskowitza <moskowitzaaron@mac.com>
2019-04-15 12:25:09 -0400

    state change

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -34,3 +34,21 @@ function googleSignout() {
                         // An error happened.
                 });
 }
+firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+                // User is signed in.
+                const { displayName } = user;
+                const { email } = user;
+                const { emailVerified } = user;
+                const { photoURL } = user;
+                const { isAnonymous } = user;
+                const { uid } = user;
+                const { providerData } = user;
+                // ...
+                console.log(email);
+                window.location('/account.html');
+        } else {
+                // User is signed out.
+                // ...
+        }
+});

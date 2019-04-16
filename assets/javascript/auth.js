9f77589a7da7a2d79c435571d705fe92e27fe580
moskowitza <moskowitzaaron@mac.com>
2019-04-16 12:52:59 -0400

    user

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -59,7 +59,7 @@
         firebase.auth().onAuthStateChanged(function(user) {
                 if (user) {
                         // User is signed in.
-                        console.log(`user ${user}`);
+                        console.log(`HEllo user ${user}`);
                         signOutBtn.classList.remove('hidden');
                         authWidget.classList.add('hidden');
                 } else {

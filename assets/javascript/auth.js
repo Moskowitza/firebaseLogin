97774464fdb04380c6a49672681208521b760747
moskowitza <moskowitzaaron@mac.com>
2019-04-16 12:18:37 -0400

    welcome

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -1,68 +1,69 @@
-// (function() {
-// Initialize Firebase
-const config = {
-        apiKey: 'AIzaSyDJnxX7y9ku7neALQG2xTqZ9tByFOfYfwo',
-        authDomain: 'loginwith-8506a.firebaseapp.com',
-        databaseURL: 'https://loginwith-8506a.firebaseio.com',
-        projectId: 'loginwith-8506a',
-        storageBucket: 'loginwith-8506a.appspot.com',
-        messagingSenderId: '437861183874',
-};
-const app = firebase.initializeApp(config);
-const uiConfig = {
-        callbacks: {
-                signInSuccessWithAuthResult(authResult, redirectUrl) {
-                        // console.log(`authResult: ${JSON.parse(authResult)}`);
-                        // console.log(`redirectUrl: ${JSON.parse(redirectUrl)}`);
-                        const { user } = authResult;
-                        const { credential } = authResult;
-                        const { isNewUser } = authResult.additionalUserInfo;
-                        const { providerId } = authResult.additionalUserInfo;
-                        const { operationType } = authResult;
-                        // Do something with the returned AuthResult.
-                        // Return type determines whether we continue the redirect automatically
-                        // console.log(`user ${JSON.parse(user)}`);
-                        // or whether we leave that to developer to handle.
-                        return true;
+(function() {
+        // Initialize Firebase
+        const config = {
+                apiKey: 'AIzaSyDJnxX7y9ku7neALQG2xTqZ9tByFOfYfwo',
+                authDomain: 'loginwith-8506a.firebaseapp.com',
+                databaseURL: 'https://loginwith-8506a.firebaseio.com',
+                projectId: 'loginwith-8506a',
+                storageBucket: 'loginwith-8506a.appspot.com',
+                messagingSenderId: '437861183874',
+        };
+        const app = firebase.initializeApp(config);
+        const uiConfig = {
+                callbacks: {
+                        signInSuccessWithAuthResult(authResult, redirectUrl) {
+                                // console.log(`authResult: ${JSON.parse(authResult)}`);
+                                // console.log(`redirectUrl: ${JSON.parse(redirectUrl)}`);
+                                const { user } = authResult;
+                                const { credential } = authResult;
+                                const { isNewUser } = authResult.additionalUserInfo;
+                                const { providerId } = authResult.additionalUserInfo;
+                                const { operationType } = authResult;
+                                // Do something with the returned AuthResult.
+                                document.getElementById('userName').innerText = user;
+                                // Return type determines whether we continue the redirect automatically
+                                // console.log(`user ${JSON.parse(user)}`);
+                                // or whether we leave that to developer to handle.
+                                return true;
+                        },
+                        signInFailure(error) {
+                                // Some unrecoverable error occurred during sign-in.
+                                // Return a promise when error handling is completed and FirebaseUI
+                                // will reset, clearing any UI. This commonly occurs for error code
+                                // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+                                // occurs. Check below for more details on this.
+                                return handleUIError(error);
+                        },
+                        uiShown() {
+                                // The widget is rendered.
+                                // Hide the loader.
+                                document.getElementById('loader').style.display = 'none';
+                        },
                 },
-                signInFailure(error) {
-                        // Some unrecoverable error occurred during sign-in.
-                        // Return a promise when error handling is completed and FirebaseUI
-                        // will reset, clearing any UI. This commonly occurs for error code
-                        // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
-                        // occurs. Check below for more details on this.
-                        return handleUIError(error);
-                },
-                uiShown() {
-                        // The widget is rendered.
-                        // Hide the loader.
-                        document.getElementById('loader').style.display = 'none';
-                },
-        },
-        signInSuccessUrl: 'https://moskowitza.github.io/firebaseLogin/account.html',
-        signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-        tosUrl: 'https://moskowitza.github.io/firebaseLogin/privacy.html',
-        privacyPolicyUrl: 'https://moskowitza.github.io/firebaseLogin/privacy.html',
-};
-const auth = app.auth();
-const ui = new firebaseui.auth.AuthUI(auth);
-ui.start(`#firebaseui-auth-container`, uiConfig);
+                signInSuccessUrl: 'https://moskowitza.github.io/firebaseLogin/account.html',
+                signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+                tosUrl: 'https://moskowitza.github.io/firebaseLogin/privacy.html',
+                privacyPolicyUrl: 'https://moskowitza.github.io/firebaseLogin/privacy.html',
+        };
+        const auth = app.auth();
+        const ui = new firebaseui.auth.AuthUI(auth);
+        ui.start(`#firebaseui-auth-container`, uiConfig);
 
-const signOutBtn = document.getElementById('signOut');
-signOutBtn.addEventListener('click', function(event) {
-        alert('sign out');
-        firebase.auth().signOut();
-});
+        const signOutBtn = document.getElementById('signOut');
+        signOutBtn.addEventListener('click', function(event) {
+                alert('sign out');
+                firebase.auth().signOut();
+        });
 
-firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-                // User is signed in.
-                console.log(user);
-                signOutBtn.classList.remove('hidden');
-        } else {
-                // No user is signed in.
-                console.log('No User');
-                signOutBtn.classList.add('hidden');
-        }
-});
-// })();
+        firebase.auth().onAuthStateChanged(function(user) {
+                if (user) {
+                        // User is signed in.
+                        console.log(user);
+                        signOutBtn.classList.remove('hidden');
+                } else {
+                        // No user is signed in.
+                        console.log('No User');
+                        signOutBtn.classList.add('hidden');
+                }
+        });
+})();

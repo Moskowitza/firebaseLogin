f145af0030acf5b479aebea94f621ae982209858
moskowitza <moskowitzaaron@mac.com>
2019-04-19 19:19:15 -0400

    again

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -94,3 +94,16 @@ auth.onAuthStateChanged(function(user) {
         }
 });
 // })();
+function loadData(data) {
+        data.forEach(item => {
+                const person = item.data();
+                console.log(`person ${JSON.stringify(person)}`);
+                const li = document.createElement('li');
+                li.innHTML = `
+                <div>${person.born}</div>
+                <div>${person.first}</div>
+                <div>${person.last}</div>
+                `;
+                dataDiv.appendChild(li);
+        });
+}

ee78f3d40f3205fad572d9efaffc30eeb23d5d1d
moskowitza <moskowitzaaron@mac.com>
2019-04-29 10:28:08 -0400

    working on modal

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,15 @@
 import { ui, uiConfig } from './config.js';
 
-ui.start(`#firebaseui-auth-container`, uiConfig);
+const loginDiv = document.getElementById('login');
+const userNav = document.getElementById('user');
+const noUserNav = document.getElementById('noUser');
+const loginModal = document.getElementById('firebaseui-auth-container');
+function showModel(event) {
+  event.preventDefault();
+  noUserNav.setAttribute('class', hidden);
+  const modal = document.createElement('div');
+  modal.setAttribute('id', 'firebaseui-auth-container');
+  document.body.append(modal);
+  ui.start(`#firebaseui-auth-container`, uiConfig);
+}
+loginDiv.addEventListener('click', showModel);

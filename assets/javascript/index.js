6cb77b9a2ec1e47276b48a476db132a1b089efd2
moskowitza <moskowitzaaron@mac.com>
2019-04-29 11:29:38 -0400

    run function

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,16 +1,18 @@
 import { ui, uiConfig } from './config.js';
 
-const loginDiv = document.getElementById('login');
-const userNav = document.getElementById('user');
-const noUserNav = document.getElementById('noUser');
-const loginModal = document.getElementById('firebaseui-auth-container');
-function showModel(event) {
-  event.preventDefault();
-  console.log('fire off modal');
-  //   noUserNav.setAttribute('class', 'hidden');
-  const modal = document.createElement('div');
-  modal.setAttribute('id', 'firebaseui-auth-container');
-  document.body.append(modal);
-  ui.start(`#firebaseui-auth-container`, uiConfig);
-}
-loginDiv.addEventListener('click', showModel);
+(function() {
+  const loginDiv = document.getElementById('login');
+  const userNav = document.getElementById('user');
+  const noUserNav = document.getElementById('noUser');
+  const loginModal = document.getElementById('firebaseui-auth-container');
+  function showModel(event) {
+    event.preventDefault();
+    console.log('fire off modal');
+    //   noUserNav.setAttribute('class', 'hidden');
+    const modal = document.createElement('div');
+    modal.setAttribute('id', 'firebaseui-auth-container');
+    document.body.append(modal);
+    ui.start(`#firebaseui-auth-container`, uiConfig);
+  }
+  loginDiv.addEventListener('click', showModel);
+})();
